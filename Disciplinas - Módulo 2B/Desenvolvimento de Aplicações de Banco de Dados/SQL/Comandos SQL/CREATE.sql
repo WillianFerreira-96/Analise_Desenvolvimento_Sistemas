a06ee2b46e5e4bc2309b07e8e066da79ec0f6264
WillianFerreira-96 <willian.ferreira.gomes96@gmail.com>
2024-12-01 15:45:09 -0300

    01/12/24

--- Disciplinas - Módulo 2B/Desenvolvimento de Aplicações de Banco de Dados/SQL/Comandos SQL/CREATE.sql
+++ Disciplinas - Módulo 2B/Desenvolvimento de Aplicações de Banco de Dados/SQL/Comandos SQL/CREATE.sql
@@ -5,5 +5,7 @@ create table nome_da_tabela(
 	nome_exemplo varchar2(20) not null,
 	data_exemplo date default sysdate,
 	cod_exemplo number(2)
+	id_tabela2 number(2)
+	constraint id_fk foreign key (id_tabela2) references tabela2(id)
 );
 

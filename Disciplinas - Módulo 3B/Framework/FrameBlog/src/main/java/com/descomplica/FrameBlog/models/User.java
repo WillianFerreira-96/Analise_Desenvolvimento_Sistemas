212261632071a9c7df7eefdc478aecf9be896659
WillianFerreira-96 <willian.ferreira.gomes96@gmail.com>
2025-04-07 00:32:24 -0300

    07/04/25

--- Disciplinas - Módulo 3B/Framework/FrameBlog/src/main/java/com/descomplica/FrameBlog/models/User.java
+++ Disciplinas - Módulo 3B/Framework/FrameBlog/src/main/java/com/descomplica/FrameBlog/models/User.java
@@ -2,10 +2,16 @@ package com.descomplica.FrameBlog.models;
 
 import com.descomplica.FrameBlog.enums.RoleEnum;
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "User")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long userId;
@@ -13,16 +19,18 @@ public class User {
     private String email;
     private String password;
     private RoleEnum role;
+    private String UserName;
 
     public User() {
     }
 
-    public User(final long userId, final String name, final String email, final String password, final RoleEnum role) {
+    public User(final long userId, final String name, final String email, final String password, final RoleEnum role, final String UserName) {
         this.userId = userId;
         this.name = name;
         this.email = email;
         this.password = password;
         this.role = role;
+        this.UserName = UserName;
     }
 
     public long getUserId() {
@@ -63,5 +71,46 @@ public class User {
 
     public void setRole(RoleEnum role) {
         this.role = role;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String UserName) {
+        this.UserName = UserName;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role == RoleEnum.ADMIN) {
+            return List.of(
+                new SimpleGrantedAuthority("ROLE_ADMIN"),
+                new SimpleGrantedAuthority("ROLE_USER")
+            );
+        }
+        return List.of(
+                new SimpleGrantedAuthority("ROLE_USER")
+        );
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
